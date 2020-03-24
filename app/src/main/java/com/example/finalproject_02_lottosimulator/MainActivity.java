cf007729b2c95b10f893ea7c9d8afe18df27db90
hjin302 <enro302@naver.com>
2020-03-24 11:19:40 +0900

    등수 카운팅 텍스트뷰 추가

--- app/src/main/java/com/example/finalproject_02_lottosimulator/MainActivity.java
+++ app/src/main/java/com/example/finalproject_02_lottosimulator/MainActivity.java
@@ -172,6 +172,24 @@ public class MainActivity extends BaseActivity {
 
         }else if (correctCount == 5){
 //            2등 / 3등 재검사 필요 => 보너스 번호를 맞췄는지?
+//            => 내 번호중에 보너스 번호와 같은게 있나?
+
+            boolean hasBonusnum = false;
+
+            for(int myNum : myLottoNumArr){
+                if(myNum == bonusNum){
+                    hasBonusnum = true;
+                    break;
+                }
+            }
+            if (hasBonusnum){
+//                2등
+                winMoneyAmount += 75000000;
+            }else{
+//                3등
+                winMoneyAmount += 1500000;
+            }
+
 
         }else if (correctCount == 4){
 //            4등
