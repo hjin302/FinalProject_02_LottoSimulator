0d52f32bec498dc781803e838bff925604a7914e
hjin302 <enro302@naver.com>
2020-03-24 09:53:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/finalproject_02_lottosimulator/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.finalproject_02_lottosimulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.finalproject_02_lottosimulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
